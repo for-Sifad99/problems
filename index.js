e71ddfbced6c3a6fdabc37215c3531b43460e1b3
Si Fad <sifayed00@gmail.com>
2025-08-19 23:59:12 +0600

    i added third problem and solution.

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 function reversedString(str) {
   let reserved = '';
 
-  // loop from last index to first
-  for (let i = str.length - 1; i >= 0 ; i--) {
-   reserved += str[i];
+  for (let i = str.length - 1; i >= 0; i--) {
+    reserved += str[i];
   };
   return reserved;
 };
@@ -15,12 +14,12 @@ reversedString('hello');// Output: "olleh"
 
 // Problem 2: Count Vowels in a String
 // Write a function that counts how many vowels(a, e, i, o, u) are in a given string.
-function countValues(str){
+function countValues(str) {
   const lowerCaseStr = str.toLowerCase();
 
   let count = 0;
   for (let i = 0; i < lowerCaseStr.length; i++) {
-    if(lowerCaseStr[i] === 'a' || lowerCaseStr[i] === 'e' || lowerCaseStr[i] === 'i' || lowerCaseStr[i] === 'o' || lowerCaseStr[i] === 'u') {
+    if (lowerCaseStr[i] === 'a' || lowerCaseStr[i] === 'e' || lowerCaseStr[i] === 'i' || lowerCaseStr[i] === 'o' || lowerCaseStr[i] === 'u') {
       count++;
     }
   };
@@ -28,3 +27,18 @@ function countValues(str){
 };
 
 countValues('Programming');// Output: 3
+
+
+// Problem 3: Check for Palindrome
+// Write a function that checks if a string is a palindrome(reads the same forward and backward).
+function isPalindrome(str) {
+  const lowerCaseStr = str.toLowerCase();
+  for (let i = 0; i < lowerCaseStr.length / 2; i++) {
+    if (lowerCaseStr[i] !== lowerCaseStr[lowerCaseStr.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+isPalindrome('madam');// Output: true
