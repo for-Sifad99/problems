3cfef5625b936382a0c76af2e924a80d8794acc5
Si Fad <sifayed00@gmail.com>
2025-08-19 23:37:33 +0600

    i added second problem and solution.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Write a function that takes a string and returns it reversed.
 function reversedString(str) {
   let reserved = '';
-  
+
   // loop from last index to first
   for (let i = str.length - 1; i >= 0 ; i--) {
    reserved += str[i];
@@ -13,4 +13,18 @@ function reversedString(str) {
 reversedString('hello');// Output: "olleh"
 
 
+// Problem 2: Count Vowels in a String
+// Write a function that counts how many vowels(a, e, i, o, u) are in a given string.
+function countValues(str){
+  const lowerCaseStr = str.toLowerCase();
 
+  let count = 0;
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+    if(lowerCaseStr[i] === 'a' || lowerCaseStr[i] === 'e' || lowerCaseStr[i] === 'i' || lowerCaseStr[i] === 'o' || lowerCaseStr[i] === 'u') {
+      count++;
+    }
+  };
+  return count;
+};
+
+countValues('Programming');// Output: 3
